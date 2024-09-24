8f25cfc2775b5d0caf81515a8d62fb6d2408d7d1
DevDouglasFiguereido <devdouglasfigueiredo@gmail.com>
2024-09-23 21:28:22 -0300

    alterações realizadas no app

---
+++ src/global/themes.tsx
@@ -0,0 +1,12 @@
+
+export const themas = {
+    Colors :{
+        primary:'#b158d1r',
+        secondary:'#ffffff',
+        lightGray:'#d7d8d7',
+        gray:'gray',
+        bgScreen:'#f1f7fa',
+        red:'#FF494C',
+        blueLigth:'#87CEF6'
+    }
+}
---
+++ src/pages/list/index.tsx
@@ -0,0 +1,79 @@
+import React,{ useState } from "react";
+import { style } from "./styles";
+import { Ball } from "../../components/Ball";
+import { Input } from "../../components/Input";
+import {MaterialIcons} from '@expo/vector-icons';
+import {Text, View,StatusBar,FlatList, TouchableOpacity} from 'react-native'
+import { Flag } from "../../components/Flag";
+import { themas } from "../../global/themes";
+
+type PropCard = {
+    item:number,
+    title:string,
+    description:string,
+    flag:'urgente'|'opcional'
+}
+
+const data:any = [
+    {
+        item:0,
+        title:'Ir ao banco !',
+        description:'página 10 a 20',
+        flag:'urgente'
+    },
+    {
+        item:1,
+        title:'Passear com cachorro!',
+        description:'página 10 a 20',
+        flag:'urgente'
+    },
+    {
+        item:2,
+        title:'Entregar Trabalho!',
+        description:'página 10 a 20',
+        flag:'urgente'
+    }
+]
+
+export default function List (){
+
+    const _renderCard = (item:PropCard,index:number) =>{
+        return (
+            <TouchableOpacity key={item.item} style={style.card}>
+                <View style={style.rowCard}>
+                    <View style={style.rowCardLeft}>
+                        <Ball color="red"/>
+                        <View>
+                            <Text style={style.titleCard}>{item.title}</Text>
+                            <Text style={style.descriptionCard}>{item.description}</Text>
+                        </View>
+                    </View>
+                    <Flag caption="Urgente" color={themas.Colors.red}/>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    
+    return(
+        <View style={style.container}>
+            <StatusBar  barStyle="light-content"/>
+            <View style={style.header}>
+                <Text style={style.greeting}>Bom dia , <Text style={{fontWeight:'bold'}}>Douglas Figueiredo.</Text></Text>
+                <View style={style.boxInput}>
+                    <Input 
+                        IconLeft={MaterialIcons}
+                        iconLeftName="search"
+                    />
+                </View>
+            </View>
+            <View style={style.boxList}>
+                <FlatList 
+                    data={data}
+                    style={{marginTop:40,paddingHorizontal:30}}
+                    keyExtractor={(item,index)=>item.number}
+                    renderItem={({item,index})=>{return(_renderCard(item,index))}}
+                />
+            </View>
+        </View>
+    )
+}
